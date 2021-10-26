9d5a3aac3646aa37c6998f16d7a9583e26cfabd4
zobotrime <93216211+zobotrime@users.noreply.github.com>
2021-10-27 01:16:22 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+	
+})
